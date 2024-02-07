042c753b38000be20b27804ca7a29820d17b10bd
josphrosales1227 <josephrosales1227@gmail.com>
2024-02-06 21:17:38 -0600

    Getting code ready for deployment

--- SecretSanta/wsgi.py
+++ SecretSanta/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SecretSanta.settings')
+settings_module = 'SecretSanta.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'SecretSanta.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
