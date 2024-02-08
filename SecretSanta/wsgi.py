e8d95c4bfa932b54e99683eb592da69d1812f527
josphrosales1227 <josephrosales1227@gmail.com>
2024-02-08 13:04:01 -0600

    Fixed imports

--- SecretSanta/wsgi.py
+++ SecretSanta/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'SecretSanta.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'SecretSanta.settings'
+settings_module = 'SecretSanta.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
