b4816c02dd6d0fcacda03edddada90c56898720b
josphrosales1227 <josephrosales1227@gmail.com>
2024-02-08 13:37:00 -0600

    Updated Settings

--- SecretSanta/settings.py
+++ SecretSanta/settings.py
@@ -7,7 +7,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -148,8 +147,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-django_heroku.settings(locals())
 
 STORAGES = {
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
