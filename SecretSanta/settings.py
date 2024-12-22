81ad3042a0ec4a2bf23bfc24be8909a44518ca9a
josphrosales1227 <josephrosales1227@gmail.com>
2024-12-22 15:01:01 -0600

    fixing static files

--- SecretSanta/settings.py
+++ SecretSanta/settings.py
@@ -122,13 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / 'static',
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
