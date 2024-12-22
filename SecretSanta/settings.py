64468901857d9f823185fbfaf566a3e60bd9f335
josphrosales1227 <josephrosales1227@gmail.com>
2024-12-22 14:25:55 -0600

    static files

--- SecretSanta/settings.py
+++ SecretSanta/settings.py
@@ -124,9 +124,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
